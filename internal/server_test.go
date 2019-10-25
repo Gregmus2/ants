99d112080339d69daf7fedd4bd5d7516f9862007
Gregmus <gregmus4@gmail.com>
2019-10-25 18:57:07 +0300

    finish tests of all general endpoints

--- internal/server_test.go
+++ internal/server_test.go
@@ -9,10 +9,13 @@ import (
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestServe(t *testing.T) {
@@ -24,7 +27,26 @@ func TestServe(t *testing.T) {
 		t.Error("size endpoint must return size from config")
 	}
 
-	registerTestRequest(t)
+	registerTestRequest(t, "Greg", "blue")
+	registerTestRequest(t, "Greg2", "green")
+
+	players := playersTestRequest(t)
+	if len(players) != 2 || players[0] != "Greg" || players[1] != "Greg2" {
+		t.Error("wrong players")
+	}
+
+	id := startTestRequest(t)
+	if id == "" {
+		t.Error("empty id from start request")
+	}
+
+	time.Sleep(1000 * time.Millisecond)
+	area := getTestRequest(t, id)
+	if len(area) != global.Config.MatchPartSize {
+		t.Error("wrong batch size")
+	}
+
+	_ = os.Remove("ants.db")
 }
 
 func pipesTestRequest(t *testing.T) []string {
@@ -50,6 +72,29 @@ func pipesTestRequest(t *testing.T) []string {
 	return pipes
 }
 
+func playersTestRequest(t *testing.T) []string {
+	ts := httptest.NewServer(http.HandlerFunc(playersHandle))
+	defer ts.Close()
+
+	res, err := ts.Client().Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	players := make([]string, 0)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = json.Unmarshal(body, &players)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return players
+}
+
 func sizeTestRequest(t *testing.T) string {
 	ts := httptest.NewServer(http.HandlerFunc(sizeHandle))
 	defer ts.Close()
@@ -67,11 +112,11 @@ func sizeTestRequest(t *testing.T) string {
 	return string(body)
 }
 
-func registerTestRequest(t *testing.T) {
+func registerTestRequest(t *testing.T, name string, color string) {
 	ts := httptest.NewServer(http.HandlerFunc(registerHandle))
 	defer ts.Close()
 
-	file, _ := os.Open("../test/test.go")
+	file, _ := os.Open("../test/" + name + ".go")
 	defer file.Close()
 
 	body := &bytes.Buffer{}
@@ -86,12 +131,12 @@ func registerTestRequest(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	err = writer.WriteField("name", "Greg")
+	err = writer.WriteField("name", name)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = writer.WriteField("color", "#000000")
+	err = writer.WriteField("color", color)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -111,4 +156,57 @@ func registerTestRequest(t *testing.T) {
 		body, _ := ioutil.ReadAll(res.Body)
 		t.Error("Body: " + string(body))
 	}
+}
+
+func startTestRequest(t *testing.T) string {
+	ts := httptest.NewServer(http.HandlerFunc(startHandle))
+	defer ts.Close()
+
+	form := url.Values{}
+	form.Add("names", "Greg,Greg2")
+
+	res, err := ts.Client().Post(ts.URL, "application/x-www-form-urlencoded", strings.NewReader(form.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Error("Wrong status code: " + strconv.Itoa(res.StatusCode))
+		body, _ := ioutil.ReadAll(res.Body)
+		t.Error("Body: " + string(body))
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return string(body)
+}
+
+func getTestRequest(t *testing.T, id string) [][][]string {
+	ts := httptest.NewServer(http.HandlerFunc(getHandle))
+	defer ts.Close()
+
+	params := url.Values{}
+	params.Add("id", id)
+	params.Add("part", "1")
+
+	res, err := ts.Client().Get(ts.URL + "?" + params.Encode())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	area := make([][][]string, 0)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = json.Unmarshal(body, &area)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return area
 }
---
+++ test/Greg2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gregmus2/ants-pkg"
+	"math/rand"
+	"time"
+)
+
+type greg2 string
+
+var r2 *rand.Rand
+
+func init() {
+	r2 = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func (g greg2) Do(fields [9]pkg.FieldType) (field uint8, action pkg.Action) {
+	field = uint8(r2.Intn(9))
+	action = pkg.ResolveAction(fields[field])
+
+	return
+}
+
+var Greg2 greg2
